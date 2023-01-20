f924e7c224e23063df686318b382a2652f06e403
Gufron Nurjalil <94749100+Gufron14@users.noreply.github.com>
2023-01-21 01:30:02 +0700

    Masukan ke  folder guru

---
+++ guru/modul/home.php
@@ -0,0 +1,65 @@
+
+    <div class="container px-6 mx-auto grid">
+        <h2 class="my-6 text-2xl font-semibold text-gray-700 dark:text-gray-200">
+            Dashboard
+        </h2>
+        <!-- CTA -->
+        <div class="flex items-center justify-between p-4 mb-8 text-sm font-semibold text-purple-100 bg-purple-600 rounded-lg shadow-md focus:outline-none focus:shadow-outline-purple" href="https://github.com/estevanmaito/windmill-dashboard">
+            <div class="flex items-center">
+                <span>Selamat datang di Sistem Pengelola Guru <?= $data['nama_lengkap'] ?></span>
+            </div>
+        </div>
+        <!-- Cards -->
+        <div class="grid gap-6 mb-8 md:grid-cols-2 xl:grid-cols-4">
+            <!-- Card Jumlah guru-->
+            <div class="flex items-center p-4 bg-white rounded-lg shadow-xs dark:bg-gray-800">
+                <div class="p-3 mr-4 text-orange-500 bg-orange-100 rounded-full dark:text-orange-100 dark:bg-orange-500">
+                    <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
+                        <path d="M13 6a3 3 0 11-6 0 3 3 0 016 0zM18 8a2 2 0 11-4 0 2 2 0 014 0zM14 15a4 4 0 00-8 0v3h8v-3zM6 8a2 2 0 11-4 0 2 2 0 014 0zM16 18v-3a5.972 5.972 0 00-.75-2.906A3.005 3.005 0 0119 15v3h-3zM4.75 12.094A5.973 5.973 0 004 15v3H1v-3a3 3 0 013.75-2.906z"></path>
+                    </svg>
+                </div>
+                <div>
+                    <p class="mb-2 text-sm font-medium text-gray-600 dark:text-gray-400">
+                        Total Kelas
+                    </p>
+                    <p class="text-lg font-semibold text-gray-700 dark:text-gray-200">
+                        <?php echo $jumlahKelas; ?>
+                    </p>
+                </div>
+            </div>
+            <!-- Card jumlah guru aktif -->
+            <div class="flex items-center p-4 bg-white rounded-lg shadow-xs dark:bg-gray-800">
+                <div class="p-3 mr-4 text-green-500 bg-green-100 rounded-full dark:text-green-100 dark:bg-green-500">
+                    <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
+                        <path d="M13 6a3 3 0 11-6 0 3 3 0 016 0zM18 8a2 2 0 11-4 0 2 2 0 014 0zM14 15a4 4 0 00-8 0v3h8v-3zM6 8a2 2 0 11-4 0 2 2 0 014 0zM16 18v-3a5.972 5.972 0 00-.75-2.906A3.005 3.005 0 0119 15v3h-3zM4.75 12.094A5.973 5.973 0 004 15v3H1v-3a3 3 0 013.75-2.906z"></path>
+                    </svg>
+                </div>
+                <div>
+                    <p class="mb-2 text-sm font-medium text-gray-600 dark:text-gray-400">
+                        Total Kelas Hari Ini
+                    </p>
+                    <p class="text-lg font-semibold text-gray-700 dark:text-gray-200">
+                        <?php echo $jumlahGuruAktif; ?>
+                    </p>
+                </div>
+            </div>
+            <!-- Card jumlah guru tidak aktif-->
+            
+        </div>
+
+        <div class="grid gap-6 mb-8 md:grid-cols-2">
+              
+              <div
+                class="min-w-0 p-4 text-white bg-purple-600 rounded-lg shadow-xs"
+              >
+                <h4 class="mb-4 font-semibold">
+                  Bahasa Indonesia
+                </h4>
+                <ul>
+                    <li>Hari/tanggal</li>
+                    <li>Jam ke </li>
+                    <li>Waktu</li>
+                    <li>Kelas</li>
+                </ul>
+              </div>
+    </div>
